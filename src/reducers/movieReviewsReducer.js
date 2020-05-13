2cf9878c20f22708dbf9ac9b99030e66a97dcbc0
Peter Hang <peterhanggg@gmail.com>
2020-05-12 20:49:37 -0400

    implemented logic to fetch movie reviews to redux store

---
+++ src/reducers/movieReviewsReducer.js
@@ -0,0 +1,20 @@
+import { GET_MOVIE_REVIEWS_SUCCESS, GET_MOVIE_REVIEWS_ERROR } from '../constants/action-types';
+
+const initialState = {
+  reviews: {},
+  loading: true,
+  error: false
+};
+
+const movieReviewsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_MOVIE_REVIEWS_SUCCESS:
+      return { ...state, reviews: action.payload, loading: false }
+    case GET_MOVIE_REVIEWS_ERROR:
+      return { ...state, loading: false, error: true }
+  default:
+    return state
+  }
+};
+
+export default movieReviewsReducer;
