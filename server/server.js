1614e7567bdf818c155b55d5ed8af9e7e580ccb8
Peter <peterhanggg@gmail.com>
2020-09-05 19:12:57 -0400

    set procile and netlify toml

--- server/server.js
+++ server/server.js
@@ -21,18 +21,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/favourite", require("./routes/favourite"));
 
-// if (process.env.ENV === "development") {
-//   // logs response status and time
-//   app.use(morgan("dev"));
-// };
+app.get('/', (req, res) => res.send('Server is up and running!'));
 
-if (process.env.ENV === "production") {
-  // Set static folders
-  app.use(express.static("../client/build"));
-
-  // sets page routes index.html
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
-};
-
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`SERVER RUNNING IN ${process.env.ENV} MODE ON PORT ${PORT}`.green.bold));
