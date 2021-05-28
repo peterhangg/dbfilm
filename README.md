e9b353d0e24ef94b7394e78edf3a07e07daed028
peter <peterhanggg@gmail.com>
2021-05-27 20:50:31 -0400

    updated readme ActorHeader component

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-## DBFIlm
-***
-Movie app to browse your favourite, popular, and upcoming release movies and cast.
-<br>
-Built with React, Redux, SASS, Node, Express, MongoDB
+# DBFIlm
+Movie database full-stack application for users to browse their favourite, popular, and upcoming release movies and cast.
 
 [Live demo](https://dbfilm.netlify.app/)
-
-### home Page
-***
-![Home page](https://media.giphy.com/media/ekjIjfdXSU5sLBMg01/giphy.gif)
-
-### Movie Info Page
-***
-![Movie Info](https://media.giphy.com/media/KHPOfAdc7w7ObXWT0w/giphy.gif)
-
-### Actor Info Page
-***
-![Actor Info](./client/src/images/actorpage.png)
+## Tech Stack
+- [Node.js](https://github.com/nodejs)
+- [Express](https://github.com/expressjs/express)
+- [MongoDB](https://github.com/mongodb/mongo)
+- [React](https://github.com/facebook/react)
+- [SASS](https://github.com/sass/sass)
--- client/src/components/ActorHeader/ActorHeader.js
+++ client/src/components/ActorHeader/ActorHeader.js
@@ -21,20 +21,22 @@ const ActorHeader = ({ actorDetails, loading ,error }) => {
 
   return (
     <div className="actor-header-container">
-      <div className="actor-header-image-wrapper">
-        <img className="actor-header-image-wrapper_image" src={actorDetails.profile_path ? `https://image.tmdb.org/t/p/h632${actorDetails.profile_path}` : ActorPlaceholder } alt="actor-profile"/>
-      </div>
-      <div className="actor-header-detals-wrapper">
-        <div className="actor-header-detals-wrapper_title-wrapper">
-          <h1 className="actor-header-detals-wrapper_name">{actorDetails.name}</h1>
-          <BackButton />
+      <div className="actor-header-container_wrapper">
+        <div className="actor-header-image-wrapper">
+          <img className="actor-header-image-wrapper_image" src={actorDetails.profile_path ? `https://image.tmdb.org/t/p/h632${actorDetails.profile_path}` : ActorPlaceholder } alt="actor-profile"/>
         </div>
-        <h3 className="actor-header-detals-wrapper_label">Biography</h3>
-        <p className="actor-header-detals-wrapper_biography">{actorDetails.biography}</p>
-        <p className="actor-header-detals-wrapper_gender"><strong>Gender:</strong> {actorDetails.gender === 2 ? "Male" : "Female"}</p>
-        <p className="actor-header-detals-wrapper_birthday"><strong>Birthday:</strong> {actorDetails.birthday}</p>
-        <p className="actor-header-detals-wrapper_place-of-birth"><strong>Place of Birth:</strong> {actorDetails.place_of_birth}</p>
-        <p className="actor-header-detals-wrapper_known-for"><strong>Known for:</strong> {actorDetails.known_for_department}</p>
+        <div className="actor-header-detals-wrapper">
+          <div className="actor-header-detals-wrapper_title-wrapper">
+            <h1 className="actor-header-detals-wrapper_name">{actorDetails.name}</h1>
+            <BackButton />
+          </div>
+          <h3 className="actor-header-detals-wrapper_label">Biography</h3>
+          <p className="actor-header-detals-wrapper_biography">{actorDetails.biography}</p>
+          <p className="actor-header-detals-wrapper_gender"><strong>Gender:</strong> {actorDetails.gender === 2 ? "Male" : "Female"}</p>
+          <p className="actor-header-detals-wrapper_birthday"><strong>Birthday:</strong> {actorDetails.birthday}</p>
+          <p className="actor-header-detals-wrapper_place-of-birth"><strong>Place of Birth:</strong> {actorDetails.place_of_birth}</p>
+          <p className="actor-header-detals-wrapper_known-for"><strong>Known for:</strong> {actorDetails.known_for_department}</p>
+        </div>
       </div>
     </div>
   )
